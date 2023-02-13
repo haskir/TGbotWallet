dac7373a0db830435e33a2f17778414ceba1525d
haskir <haskird2@gmail.com>
2023-02-13 17:45:48 +0300

    created GoogleHandler class

--- google_handler.py
+++ google_handler.py
@@ -1,68 +1,84 @@
-from __future__ import print_function
+from typing import Optional
 import os.path
-import httplib2
-import apiclient
-from oauth2client.service_account import ServiceAccountCredentials
+import googleapiclient.discovery
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
-          'https://www.googleapis.com/auth/drive']
+class GoogleHandler:
+    Exist = False
 
+    def __new__(cls, *args, **kwargs):
+        """ SINGLETON """
+        if not GoogleHandler.Exist:
+            GoogleHandler.Exist = super().__new__(cls)
+            return GoogleHandler.Exist
+        return GoogleHandler.Exist
 
-def main():
-    with open("cred.ini", "r") as file:
-        configs = {}
-        for string in file.readlines():
-            configs[string.split("=")[0]] = string.split("=")[1].rstrip()
-        # print(f"{configs=}")
+    def __init__(self, path_to_ini: str = "cred.ini", scopes=None):
+        self.service_inner = None
+        if scopes is None:
+            scopes = ['https://www.googleapis.com/auth/spreadsheets',
+                      'https://www.googleapis.com/auth/drive']
+        with open(path_to_ini, "r") as file:
+            self.configs = dict()
+            for string in file.readlines():
+                self.configs[string.split("=")[0]] = string.split("=")[1].rstrip()
 
-    SAMPLE_SPREADSHEET_ID = configs.get("sheet_id")
-    CREDENTIALS_FILE = configs.get("account_creds")
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, SCOPES)
-    httpauth = credentials.authorize(httplib2.Http())
-    service = apiclient.discovery.build("sheets", "v4", http=httpauth)
+        self._SAMPLE_SPREADSHEET_ID = self.configs.get("sheet_id")
+        self._CREDENTIALS_FILE = self.configs.get("account_creds")
 
-    values = service.spreadsheets().values().get(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID,
-        range="A1:E10",
-        majorDimension="ROWS"
-    ).execute()
-    print(values)
+    def connect(self, output: bool = False) -> googleapiclient.discovery.build:
+        """If output == True prints array of files"""
+        creds, _ = google.auth.load_credentials_from_file(self._CREDENTIALS_FILE)
+        self.service_inner = build(serviceName='drive', version='v3', credentials=creds)
+        files = []
 
-    # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     print(1)
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #         print(2)
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             'credentials.json', SCOPES)
-    #         creds = flow.run_local_server(port=0)
-    #     # Save the credentials for the next run
-    #     with open('token.json', 'w') as token:
-    #         token.write(creds.to_json())
-    #
-    # try:
-    #     service = build('sheets', 'v4', credentials=creds)
-    #
-    #     # Call the Sheets API
-    #     sheet = service.spreadsheets()
-    #     result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-    #                                 range=SAMPLE_RANGE_NAME).execute()
-    #     values = result.get('values', [])
-    #
-    #     if not values:
-    #         print('No data found.')
-    #         return
-    #
-    #     print('Name, Major:')
-    #     for row in values:
-    #         # Print columns A and E, which correspond to indices 0 and 4.
-    #         print('%s, %s' % (row[0], row[4]))
-    # except HttpError as err:
-    #     print(err)
+        if output:
+            print(self.show_files())
+
+    def show_files(self) -> list:
+        files = []
+        page_token = None
+        while True:
+            # pylint: disable=maybe-no-member
+            response = self.service_inner.files().list(
+                fields='nextPageToken, files(id, name)',
+                pageToken=page_token).execute()
+            files.extend(response.get('files', []))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return files
+
+    def create_folder(self, names_for_folders: str) -> bool:
+        try:
+            file_metadata = {
+                'name': names_for_folders,
+                'mimeType': 'application/vnd.google-apps.folder'
+            }
+            self.service_inner.files().create(body=file_metadata, fields='id').execute()
+        except HttpError as error:
+            print(F'An error occurred: {error}')
+            return False
+        return True
+
+    def delete_file(self, name_for_file: str) -> bool:
+        try:
+            file_metadata = {
+                'name': name_for_file,
+                'mimeType': 'application/vnd.google-apps.folder'
+            }
+            self.service_inner.files().create(body=file_metadata, fields='id').execute()
+        except HttpError as error:
+            print(F'An error occurred: {error}')
+            return False
+        return True
 
 
 if __name__ == '__main__':
-    main()
+    service = GoogleHandler()
+    service.connect()
+    # service.create_folder("Test_Folder")
+    print(service.show_files())
