b0fd978638d85e4f3ea97a563d18ebfc52b529d0
haskir <haskird2@gmail.com>
2023-02-26 23:21:41 +0300

    created pre-alpha db

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+import os
+import dotenv
+dotenv.load_dotenv()
 
-
-API_TOKEN: str = '5697945722:AAEbTbpT11PwB_j1vFn_9MN6nQ19SrVFfqk'
+API_TOKEN: str = os.getenv('BOT_TOKEN') 
 
 # Создаем объекты бота и диспетчера
 bot: Bot = Bot(token=API_TOKEN)
-dp: Dispatcher = Dispatcher()
+dp: Dispatcher = Dispatcher(bot=bot)
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -17,18 +19,23 @@ async def process_start_command(message: Message):
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
+@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
 
 
 if __name__ == '__main__':
     dp.run_polling(bot)
+    ...
