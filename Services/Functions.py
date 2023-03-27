32818595ebc40d703e9aae80d1b67c9c24a5d2da
haskir <haskird2@gmail.com>
2023-03-27 15:47:47 +0300

    sss

--- Services/Functions.py
+++ Services/Functions.py
@@ -42,8 +42,8 @@ def show_payments(user: User | str | int,
         result = [Payment(*string) for string in payments_handler.show_payments(sheet_id)]
         if sort is not None:
             result = payments_handler.sort(sheet_id,
-                                                 sort[0],
-                                                 sort[1])
+                                           sort[0],
+                                           sort[1])
         return "".join(str(payment) for payment in result)
     else:
         return "Пока что пусто\n"
